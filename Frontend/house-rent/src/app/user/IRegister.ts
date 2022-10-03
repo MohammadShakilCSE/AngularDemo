5691783be080122a56b81cad045b8a17a48a0d0c
Mohammad Shakil <shakiltalukder.seu19@gmail.com>
2022-10-03 13:37:24 +0600

    Registration form with validation

---
+++ Frontend/house-rent/src/app/user/IRegister.ts
@@ -0,0 +1,7 @@
+export interface IRegister{
+    UserName:string,
+    Email:string,
+    Password:string,
+    ConfirmPassword:string,
+    Mobile:string
+}
--- Frontend/house-rent/src/app/user/register/register.component.ts
+++ Frontend/house-rent/src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {ConfirmPassword} from './confirm-password.validators';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registrationForm=new FormGroup({
+    UserName :new FormControl(),
+    Email :new FormControl(),
+    Password: new FormControl(),
+    ConfirmPassword: new FormControl(),
+    mobile:new FormControl(),
+   });
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.registrationForm= this.fb.group ({
+      UserName :new FormControl('',[Validators.required,Validators.minLength(3)]),
+      Email:new FormControl(null,[Validators.required,Validators.email]),
+      Password:new FormControl(null,[Validators.required,Validators.minLength(6)]),
+      ConfirmPassword:new FormControl(null,[Validators.required]),
+     mobile:new FormControl(null,[Validators.required,Validators.maxLength(11)]),
+    },{
+      validator: ConfirmPassword("Password", "ConfirmPassword")
+    });
+  }
+  //get methods
+  get UserName(){
+    return this.registrationForm.get('UserName');
+
+  }
+  get Email(){
+    return this.registrationForm.get('Email');
+  }
+  get Password(){
+    return this.registrationForm.get('Password');
+  }
+  get ConfirmPassword(){
+    return this.registrationForm.get('ConfirmPassword');
+  }
+  get mobile(){
+    return this.registrationForm.get('mobile');
+  }
+
+  onSubmit(){
+    console.log(this.registrationForm);
   }
 
 }
